987cce36ee93124c5f73af3139f83263ef3585a6
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-01-16 16:14:01 -0800

    Final test
    This is the last test I'll  be doing today

--- default.rb
+++ default.rb
@@ -7,5 +7,5 @@
 # All rights reserved - Do Not Redistribute
 #
 file '/etc/motd' do
-       content 'Test completed successfully'
+       content 'This most be shown if everything went fine'
 end
--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'test'
 maintainer       'Softtek'
 maintainer_email 'luis.mendoza@softtek.com'
 license          'All rights reserved'
-description      'Installs/Configures test'
+description      'Shows a single message at /etc/motd on target host'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
