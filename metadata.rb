154a137c114f15ea4a80d523b3b9971128b1c3ee
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-02-02 10:15:14 -0800

    First complete test
    This is merely a test for new cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'test'
+name             'oraclient-install'
 maintainer       'Softtek'
 maintainer_email 'luis.mendoza@softtek.com'
 license          'All rights reserved'
-description      'Shows a single message at /etc/motd on target host'
+description      'Installs/Configures oracle sql client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.8'
---
+++ table_insertion.rb
@@ -0,0 +1,17 @@
+
+# Cookbook Name:: oraclient-install
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+bash 'Select all users from dba_users' do             
+  code <<-EOH
+    sqlplus64 system/LetsDocker@sql-server:1521/ORCLPDB1 <<-SQLEOH
+    INSERT INTO demo_prueba1 (id, data)
+    VALUES (66, 'prueba para el demo');
+    exit;
+    SQLEOH
+  EOH
+end
