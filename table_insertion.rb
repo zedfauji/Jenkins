96b8d6368bb83ea9d65a6cc17b356f774934bac4
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-02-15 17:15:48 -0800

    QA
    QA push

--- table_insertion.rb
+++ table_insertion.rb
@@ -9,7 +9,7 @@
 bash 'Select all users from dba_users' do             
   code <<-EOH
     sqlplus64 system/LetsDocker@sql-server:1521/ORCLPDB1 <<-SQLEOH
-    INSERT INTO demo_dev_env (id, data)
+    INSERT INTO demo_qa_env (id, data)
     VALUES (66, 'prueba para el demo');
     exit;
     SQLEOH
