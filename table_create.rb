9d7383549196c9b0949971a1925b24e3705c9ea5
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-02-15 16:26:29 -0800

    Environment test
    Test for environment cookbooks constraint

--- table_create.rb
+++ table_create.rb
@@ -9,7 +9,7 @@
 bash 'Select all users from dba_users' do             
   code <<-EOH
     sqlplus64 system/LetsDocker@sql-server:1521/ORCLPDB1 <<-SQLEOH
-    CREATE TABLE demo_prueba1(
+    CREATE TABLE demo_prueba_env(
     id    NUMBER(10),
     data  VARCHAR2(50)
     );
