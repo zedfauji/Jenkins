f67074d0bdc61cad1f0049c7d8c3e39737b24690
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-02-17 10:11:58 -0800

    Dev
    Wring table to be created/edited so it can be shown during Demo how to
    set it correctly

--- table_insertion.rb
+++ table_insertion.rb
@@ -9,7 +9,7 @@
 bash 'Select all users from dba_users' do             
   code <<-EOH
     sqlplus64 system/LetsDocker@sql-server:1521/ORCLPDB1 <<-SQLEOH
-    INSERT INTO demo_dev_env (id, data)
+    INSERT INTO demo_dev_env1 (id, data)
     VALUES (66, 'Esta prueba es para los ambientes de Desarrollo');
     exit;
     SQLEOH
