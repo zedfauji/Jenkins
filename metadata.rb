32bf9d4b4793065fcfba6ab21be13bed7909e40b
FlipMendoza <luis.felipex.mendoza.orozco@intel.com>
2017-02-22 12:08:45 -0800

    Updating
    Updating new usage for this repository

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'oraclient-install'
-maintainer       'Softtek'
-maintainer_email 'luis.mendoza@softtek.com'
-license          'All rights reserved'
-description      'Installs/Configures oracle sql client'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
